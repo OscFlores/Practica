ccde300defc980d34e5f5325fd103c211e73b927
Osc <reosc456@gmail.com>
2021-08-05 22:34:10 -0600

    Inicializacion

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,20 @@
+package Main;
+import Identificador.Identificador;
+/**
+ *
+ * @author Oscar
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Identificador n = new Identificador();
+        n.setVisible(true);
+        n.setLocationRelativeTo(null);
+        n.setTitle("Identificador");
+    }
+    
+}
